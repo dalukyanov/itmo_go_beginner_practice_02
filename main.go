00dd7091a09109a4e4137a8cd4619ccb1b9892af
Dmitry Lukyanov <hanatsu.tori@gmail.com>
2025-10-04 19:24:39 +0300

    test_02

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Test")
+	fmt.Println("Test 1")
+	fmt.Println("Test 2")
+	fmt.Println("Test 3")
+	fmt.Fprintf(os.Stderr, "%s\n", "Test Stderr")
+	fmt.Fprintf(os.Stderr, "%s\n", "Test Stdout")
+
 }
